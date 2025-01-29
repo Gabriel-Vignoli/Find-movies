bc6c24951f8ad56922973614e03473c175ba1fe7
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2025-01-29 20:04:51 -0300

    chore: initialize project with React, Tailwind CSS, and assets

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <main>
+      <div className="pattern"></div>
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies </span> You'll Enjoy Withou thw Hassle</h1>
+        </header>
+      </div>
+    </main>
+  );
+};
+
+export default App;
